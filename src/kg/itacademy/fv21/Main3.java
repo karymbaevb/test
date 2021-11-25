606fa744f3dbd2e6b3bf89be55d54fce734669b9
bkarymbaev <bkarymbaev@gmail.com>
2021-11-25 14:10:57 +0600

    test init

---
+++ src/kg/itacademy/fv21/Main3.java
@@ -0,0 +1,8 @@
+package kg.itacademy.fv21;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/kg/itacademy/fv21/Main4.java
@@ -0,0 +1,8 @@
+package kg.itacademy.fv21;
+
+public class Main4 {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
